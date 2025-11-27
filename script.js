ba2c9bcd222bf28d4509d01e115dbe1137ea0ad1
Kam Rodriguez <kamrod1021@gmail.com>
2025-11-26 19:43:45 -0800

    I added comments explaining what querySelector/All do, and I began adding to my JS

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+// document.querySelector() will return the first match of what you are looking for. 
+// document.querySelectorAll() will return a nodelist. It can select through all the elemetns that have the same class name.
+// For example the below code finds the first game-square div and makes it red. 
+
+
+
+const gameSquares = document.querySelectorAll('.game-square'); 
+const playAgain = document.querySelector("#play-again");
+const winConditions =
+]
