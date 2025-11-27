18ae4341925a05140b555afb184cd679ce3aff9e
Kam Rodriguez <kamrod1021@gmail.com>
2025-11-26 19:50:39 -0800

    I finished adding the win conditions.

--- script.js
+++ script.js
@@ -9,5 +9,13 @@
 
 const gameSquares = document.querySelectorAll('.game-square'); 
 const playAgain = document.querySelector("#play-again");
-const winConditions =
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
 ]
