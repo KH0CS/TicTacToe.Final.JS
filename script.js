c0549ccbd38a5e0fffa5104da80146d1e6aef414
Kam Rodriguez <kamrod1021@gmail.com>
2025-11-26 20:02:01 -0800

    Saving.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 const gameSquares = document.querySelectorAll('.game-square'); 
 const playAgain = document.querySelector("#play-again");
 const winConditions = [
-  [0, 1, 2],
+  [0, 1, 2], 
   [3, 4, 5],
   [6, 7, 8],
   [0, 4, 8],
